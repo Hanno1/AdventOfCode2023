6507e781f31a270e7932b777a95cbafbdd37df7c
Hanno1 <hanno.barschel@web.de>
2023-12-06 09:53:04 +0100

    main solved day 5

--- 05_2023.py
+++ 05_2023.py
@@ -47,33 +47,39 @@ def first_task():
 
 def map_with_range(entry, mapping):
     start_seed, range_ = entry
-    resulting_seeds = []
-    rest_seeds = []
+    final_seed = start_seed + range_ - 1
     for dest_m, source_m, range_m in mapping:
+        final_m = source_m + range_m - 1
         # 4 possibilities:
         # 1) start seed in range
         if source_m <= start_seed < source_m + range_m:
             # 1.1) range in range as well, no rest seeds
             if start_seed + range_ <= source_m + range_m:
-                resulting_seeds.append([dest_m + (start_seed - source_m), range_])
+                return [[dest_m + (start_seed - source_m), range_]]
             # 1.2) range is not in range
             else:
-                rest_startseed = source_m + range_ + 1
-                rest_range = range_ - (rest_startseed - start_seed)
+                rest_startseed = final_m + 1
+                rest_range = final_seed - final_m
+                res = map_with_range([rest_startseed, rest_range], mapping)
 
-                print(rest_startseed, rest_range)
+                return [[dest_m + (start_seed - source_m), final_m - start_seed + 1]] + res
+        # 2) start seed not in range but final seed is in range:
+        elif source_m <= final_seed <= final_m:
+            rest_range = source_m - start_seed
+            res = map_with_range([start_seed, rest_range], mapping)
 
-                resulting_seeds.append([dest_m + (start_seed - source_m), (rest_startseed - start_seed)])
-                res, rest = map_with_range([rest_startseed, rest_range], mapping)
-                resulting_seeds.append(res)
-                rest_seeds.append(rest)
-        # 2) start seed not in range -> start seed bigger
-        elif start_seed >= source_m + range_m:
-            resulting_seeds.append([dest_m + range_m, range_])
-            
-    if rest_seeds != []:
-        resulting_seeds += rest_seeds
-    return resulting_seeds, rest_seeds
+            return [[dest_m, final_seed - source_m + 1]] + res
+
+        # 3) start seed not in range and final seed not in range, but middle in range:
+        elif start_seed < source_m and final_m < final_seed:
+            first_range = source_m - start_seed
+            second_range = final_seed - final_m
+
+            res1 = map_with_range([start_seed, first_range], mapping)
+            res2 = map_with_range([final_m + 1, second_range], mapping)
+
+            return [[dest_m, range_m]] + res1 + res2
+    return [entry]
 
 def second_task():
     data = hc.read_file_line("05_2023")
@@ -83,14 +89,22 @@ def second_task():
         seed = mat[0][entry]
         range_ = mat[0][entry + 1]
         actual_seeds = [[seed, range_]]
-        for act_seed in actual_seeds:
-            pass
+        for i in range(1, len(mat)):
+            next_seeds = []
+            for j in range(len(actual_seeds)):
+                next_seeds += map_with_range(actual_seeds[j], mat[i])
+            actual_seeds = next_seeds
+        # print(actual_seeds)
+        for seed_c, _ in actual_seeds:
+            if seed_c < lowest_loc:
+                lowest_loc = seed_c
+    # 260990830 - too high
     print(lowest_loc)
 
 # first_task()
-# second_task()
+second_task()
 
-seed = [2, 4]
-mapping = [[1, 0, 5]]
-print(map_with_range(seed, mapping))
+# seed = [9, 10]
+# mapping = [[11, 10, 10]]
+# print(map_with_range(seed, mapping))
     
